0c3eba71cfc817307b34c8550b88aede194534ef
PC32 ENGINEERING3FLN <terry.chan@hohokusinc.com>
2020-09-23 17:51:58 -0400

    added sorting functionality

--- server/__main__.py
+++ server/__main__.py
@@ -3,13 +3,14 @@
 import connexion
 
 from server import encoder
-
+from flask_cors import CORS
 
 def main():
     app = connexion.App(__name__, specification_dir='./swagger/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'Status Microservice'}, pythonic_params=True)
-    app.run(port=8080)
+    CORS(app.app)
+    app.run(port=8083)
 
 
 if __name__ == '__main__':
--- server/controllers/status_controller.py
+++ server/controllers/status_controller.py
@@ -10,52 +10,47 @@ from datetime import datetime
 from server.models.status_info import StatusInfo  # noqa: E501
 from server import util
 
-file = "/usr/src/app/doc/demo.xlsx"
-def get_statusnj():  # noqa: E501
+# file = "/usr/src/app/doc/kaizen_board.xlsx"
+file = "C:/Users/ENGINEERING-FLN1/Documents/Automation/Projects/software_ws/demo_shared_drive/kaizen_board.xlsx"
+
+def get_my_key(obj,sorting):
+    return obj[sorting]
+
+def get_statusnj(sorting):  # noqa: E501
     """Get all status at Paterson
 
      # noqa: E501
 
+    :param sorting: This is getting the suggestion status with sorting order
+    :type sorting: str
 
     :rtype: List[StatusInfo]
     """
-
-    # file = "C:/Users/ENGINEERING-FLN1/Documents/Automation/Projects/software_ws/demo_shared_drive/demo.xlsx"
-    
     df = pd.read_excel(file, sheet_name='NJ')
 
     result = df.to_json(orient="records")
     df_json = json.loads(result)
 
     for i in range(len(df_json)):
+        df_json[i]["ticket_num"] = df_json[i].pop("Ticket Number")
+        df_json[i]["suggestion"] = df_json[i].pop("Suggestion")
+        df_json[i]["req_person"] = df_json[i].pop("Requested person")
+        df_json[i]["res_person"] = df_json[i].pop("Responsible person")
+        df_json[i]["status"] = df_json[i].pop("Status")
+        df_json[i]["urgency"] = df_json[i].pop("Urgency")
         try:
-            timestamp = df_json[i]["Due"]/1000
-            df_json[i]["Due"] = datetime.fromtimestamp(timestamp).isoformat()
+            timestamp = df_json[i]["Date added"]/1000
+            date = datetime.fromtimestamp(timestamp)
+            df_json[i]["date_added"] = date.strftime("%m/%d/%Y")
+            df_json[i].pop("Date added")
+            logger.debug(type(df_json[i]["date_added"]))
         except:
             logger.debug("This is not a timestamp")
     logger.debug(df_json)
-    return df_json
-
-
-def get_statusva():  # noqa: E501
-    """Get all status at Richmond
-
-     # noqa: E501
-
-
-    :rtype: List[StatusInfo]
-    """
-    
-    df = pd.read_excel(file, sheet_name='VA')
-    result = df.to_json(orient="records")
-    df_json = json.loads(result)
-
-    for i in range(len(df_json)):
-        try:
-            timestamp = df_json[i]["Due"]/1000
-            df_json[i]["Due"] = datetime.fromtimestamp(timestamp).isoformat()
-        except:
-            logger.debug("This is not a timestamp")
-    logger.debug(df_json)
-    return df_json
+    # df_json.sort(key=get_my_key(sorting))
+    if sorting == "urgency":
+        return_json = sorted(df_json,key=lambda i:i[sorting], reverse=True)
+    else:
+        return_json = sorted(df_json,key=lambda i:i[sorting])
+    return return_json
 
